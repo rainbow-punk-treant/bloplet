1235e492c4656dcdb206e6c5b7a166c9659cd8ee
Entropy <benbelland@gmail.com>
2020-12-05 14:07:22 -0800

    dumpster droppin

--- super/js/script.js
+++ super/js/script.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
 
+    var doc = $('container');
+    document.addEventListener("drag", function(event) {
+      doc.css.color = "red";
+    });
+    document.addEventListener("dragenter", function(event) {
+      doc.css.border = "dotted";
+    });
+    document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    if ( event.target.className == "droptarget" ) {
+        demo.css.color = "";
+        event.target.css.border = "";
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
+});
     baguetteBox.run('.gallery');
 }); //this is a callback! A self contained system of,
 //when the document is ready, execute this anonymous
